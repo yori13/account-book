563213db2bf611fc1eed36c6d79cc2e5e47c0c3b
yori <yorirugby13@gmail.com>
2024-08-21 20:38:15 +0900

    ヘッダー追加

--- react/src/App.js
+++ react/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import TopPage from './components/pages/TopPage';
-import CashbookPage from './components/pages/CashbookPage'; // 現金出納帳画面（仮）
-import CreditbookPage from './components/pages/CreditbookPage'; // クレジット出納帳画面（仮）
+import TopPage from './components/pages/TopPage'; // TOPページ
+import CashbookPage from './components/pages/CashbookPage'; // 現金出納帳画面
+import CreditbookPage from './components/pages/CreditbookPage'; // クレジット出納帳画面
+import HeaderCompornent from '../header/header'; // ヘッダー
+
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <HeaderCompornent />
         <Routes>
           <Route path="/" element={<TopPage />} />
           <Route path="/cashbook" element={<CashbookPage />} />
---
+++ react/src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeaderCompornent = ()=>{
+  return(
+    <>
+      <header>
+        <h1>出納帳アプリ</h1>
+        <button>ログアウト</button>
+      </header>
+    </>
+  );
+}
+
+export default HeaderCompornent;
