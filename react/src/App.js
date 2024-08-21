25755e221a1437d414875c73428480484c68fe9c
yori <yorirugby13@gmail.com>
2024-08-21 20:39:20 +0900

    パス修正

--- react/src/App.js
+++ react/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TopPage from './components/pages/TopPage'; // TOPページ
 import CashbookPage from './components/pages/CashbookPage'; // 現金出納帳画面
 import CreditbookPage from './components/pages/CreditbookPage'; // クレジット出納帳画面
-import HeaderCompornent from '../header/header'; // ヘッダー
+import HeaderCompornent from './components/header/header'; // ヘッダー
 
 
 function App() {
