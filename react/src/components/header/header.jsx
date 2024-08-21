51a7b52a9382494fb677435069d4d93be4502b1d
yori <yorirugby13@gmail.com>
2024-08-21 21:32:02 +0900

    ヘッダーとトップのデザイン

--- react/src/components/header/header.jsx
+++ react/src/components/header/header.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const HeaderCompornent = ()=>{
   return(
     <>
-      <header>
-        <h1>出納帳アプリ</h1>
-        <button>ログアウト</button>
-      </header>
+  <header className="bg-blue-900 p-5">
+    <div className="flex items-center justify-between">
+      <h1 className="text-xl font-bold text-white">出納帳アプリ</h1>
+      <button className="text-white">ログアウト</button>
+    </div>
+  </header>
+
     </>
   );
 }
