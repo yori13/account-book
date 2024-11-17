d12de381e875bcd2e83e04db8f3c34add307f73f
yori <yorirugby13@gmail.com>
2024-11-17 22:10:06 +0900

    nginx追加

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/pages/LoginPage';
 import TopPage from './components/pages/TopPage'; // TOPページ
 import CashbookPage from './components/pages/CashbookPage'; // 現金出納帳画面
 import CreditbookPage from './components/pages/CreditbookPage'; // クレジット出納帳画面
 import CashConfirmation from './components/pages/CashConfirmation'
 import HeaderCompornent from './components/header/header'; // ヘッダー
 import CreditConfirmation from './components/pages/CreditConfirmation';
-import Edit from './components/pages/EditPage';
+import EditPage from './components/pages/EditPage';
 
 
 function App() {
@@ -15,12 +16,13 @@ function App() {
       <div className="App">
         <HeaderCompornent />
         <Routes>
-          <Route path="/" element={<TopPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/TopPage" element={<TopPage />} />
           <Route path="/cashbook" element={<CashbookPage />} />
           <Route path="/creditbook" element={<CreditbookPage />} />
           <Route path='/CashConfirmation' element={<CashConfirmation/>}/>
           <Route path='/CreditConfirmation' element={<CreditConfirmation/>}/>
-          <Route path='/Edit' element={<Edit/>}/>
+          <Route path='/EditPage' element={<EditPage/>}/>
         </Routes>
       </div>
     </Router>
---
+++ react/src/components/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <>
+      <div>
+        LoginPage
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
