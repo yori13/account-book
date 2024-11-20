bb017688d912250bd31af7b7d392dff6c4611470
yori <yorirugby13@gmail.com>
2024-11-20 10:45:52 +0900

    ルーティング修正

--- api/routes/creditAccount.js
+++ api/routes/creditAccount.js
@@ -1,95 +1,65 @@
 const express = require('express');
-const router = express.Router(); // これでrouterを定義
+const router = express.Router();
 const { creditAccount, creditDetail, sequelize } = require('../models');
 
 router.post('/', async (req, res) => {
   const t = await sequelize.transaction();
   try {
-    const date = req.body.date;
-    const gasoline = req.body.gasoline;
-    const phone = req.body.phone;
-    const uniform = req.body.uniform;
-    const material = req.body.material;
-    const etc = req.body.etc;
-    const other = req.body.other;
-    const detail = req.body.detail;
-    // const userCode = req.body.user_code;
-    const userCode = 1;
+    const { date, gasoline, phone, uniform, material, etc, other, detail } = req.body;
+    const userCode = 1;  // 仮のユーザーコード
 
     let creditDetailId = null;
 
-    if (other) {
+    if (!other) {
+      other = 0;
+    }
+
+    // creditDetailの挿入と即座のコミット
+    try {
       const newDetail = await creditDetail.create({
         detail: detail ? detail : "特になし"
       }, { transaction: t });
-
-      creditDetailId = newDetail.id;
+      creditDetailId = newDetail.id;  // creditDetailのIDを取得
+      console.log('test::' + creditDetailId);
+      
+      // creditDetailが挿入されて、IDが確定した時点でコミット
+      await t.commit();
+    } catch (error) {
+      await t.rollback();
+      console.error('Error inserting creditDetail:', error.message);
+      return res.status(500).send(`Error inserting creditDetail: ${error.message}`);
     }
 
+    // creditDetailが挿入された後、IDを使って次の処理を行う
     const sendData = [];
+    const categories = [
+      { value: gasoline, categoryCode: 1 },
+      { value: phone, categoryCode: 2 },
+      { value: uniform, categoryCode: 3 },
+      { value: material, categoryCode: 4 },
+      { value: etc, categoryCode: 5 },
+      { value: other, categoryCode: 6, categoryDetailCode: creditDetailId }
+    ];
 
-    if(gasoline){
-      sendData.push({
-        date: date,
-        credit_price: gasoline,
-        category_code: 1,
-        category_detail_code: 0,
-        user_code: userCode
-      });
-    }
+    categories.forEach(item => {
+      if (item.value) {
+        sendData.push({
+          date: date,
+          credit_price: item.value,
+          category_code: item.categoryCode,
+          category_detail_code: item.categoryDetailCode || 0,
+          user_code: userCode
+        });
+      }
+    });
 
-    if(phone){
-      sendData.push({
-        date: date,
-        credit_price: phone,
-        category_code: 2,
-        category_detail_code: 0,
-        user_code: userCode
-      });
-    }
+    // creditAccountのbulkCreate
+    await creditAccount.bulkCreate(sendData, {
+      transaction: t,
+      attributes: ['date', 'credit_price', 'category_code', 'category_detail_code', 'user_code']
+    });
 
-    if(uniform){
-      sendData.push({
-        date: date,
-        credit_price: uniform,
-        category_code: 3,
-        category_detail_code: 0,
-        user_code: userCode
-      });
-    }
-
-    if(material){
-      sendData.push({
-        date: date,
-        credit_price: material,
-        category_code: 4,
-        category_detail_code: 0,
-        user_code: userCode
-      });
-    }
-
-    if(etc){
-      sendData.push({
-        date: date,
-        credit_price: etc,
-        category_code: 5,
-        category_detail_code: 0,
-        user_code: userCode
-      });
-    }
-
-    if(other){
-      sendData.push({
-        date: date,
-        credit_price: other,
-        category_code: 6,
-        category_detail_code: creditDetailId,
-        user_code: userCode
-      });
-    }
-
-    await creditAccount.bulkCreate(sendData, { transaction: t });
-
+    // トランザクションをコミット
     await t.commit();
     res.status(200).send("Data inserted successfully");
 
