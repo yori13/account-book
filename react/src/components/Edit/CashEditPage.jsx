ad0ce1fdafba81eb3df194d4bb010c7fb4086da3
yori <yorirugby13@gmail.com>
2024-11-20 20:45:27 +0900

    現金側とクレジット側をタブで切り替え

---
+++ react/src/components/Edit/CashEditPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const CashEditPage = () => {
+  return(
+    <>
+      <table>
+        <thead>
+          <th>日付</th>
+          <th>費目</th>
+          <th>摘要</th>
+          <th>金額項目</th>
+          <th>金額</th>
+          <th>操作</th>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default CashEditPage;
