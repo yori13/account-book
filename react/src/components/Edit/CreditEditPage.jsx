a918509aac2dae7d27dddcb3cb72a011d879a35a
yori <yorirugby13@gmail.com>
2024-11-21 22:54:51 +0900

    thのデザインを追加

--- react/src/components/Edit/CreditEditPage.jsx
+++ react/src/components/Edit/CreditEditPage.jsx
@@ -3,20 +3,24 @@ import React from "react";
 const CreditEditPage = () => {
   return(
     <>
-      <table>
-        <thead>
-          <th>ガソリン代</th>
-          <th>携帯電話代</th>
-          <th>作業着代</th>
-          <th>材料費</th>
-          <th>ETC</th>
-          <th>その他</th>
-          <th>備考</th>
-          <th>操作</th>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
+      <div className="overflow-x-auto">
+        <table className="table-fixed w-full border-collapse border border-gray-300">
+          <thead>
+            <tr>
+              <th className="w-1/8 border px-4 py-2">ガソリン代</th>
+              <th className="w-1/8 border px-4 py-2">携帯電話代</th>
+              <th className="w-1/8 border px-4 py-2">作業着代</th>
+              <th className="w-1/8 border px-4 py-2">材料費</th>
+              <th className="w-1/8 border px-4 py-2">ETC</th>
+              <th className="w-1/8 border px-4 py-2">その他</th>
+              <th className="w-1/8 border px-4 py-2">備考</th>
+              <th className="w-1/8 border px-4 py-2">操作</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
