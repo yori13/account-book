be92f8b6c86b450b93f0bc0cb94e8e1dda219c58
yori <yorirugby13@gmail.com>
2024-12-02 12:25:26 +0900

    編集画面デザイン

--- react/src/components/Edit/CashEditInput.jsx
+++ react/src/components/Edit/CashEditInput.jsx
@@ -40,10 +40,11 @@ const CashEditInput = () => {
         dispatch(CashReset());
         navigate('/');
       }else{
-        alert('データ更新に失敗しました' + response.status);
+        alert('データ更新に失敗しました：' + response.status);
         console.log(response.status);
       }
     } catch (error) {
+      alert('データ更新に失敗しました：' + error.message);
       console.error("Error submitting data:", error);
     }
   };
@@ -51,16 +52,19 @@ const CashEditInput = () => {
   return (
     <>
       <HeaderCompornent />
-      <div>
+      <div className="flex justify-center">
+        <h1 className="text-xl mt-5 mb-5">編集入力画面</h1>
+      </div>
+      <div className="flex justify-center">
         <form onSubmit={handleSubmit(onSubmit)}>
           <Controller name="id" control={control} render={({field}) => <input type="hidden" {...field}/>} />
-          <div>
+          <div className="text-left space-y-5 mb-10">
             <div>
               <label htmlFor="date">日付：</label>
               <Controller
                 name="date"
                 control={control}
-                render={({ field }) => <input type="text" {...field} readOnly />}
+                render={({ field }) => <input type="text" className="border border-brack h-10 w-44" {...field} readOnly />}
               />
             </div>
             <div>
@@ -69,7 +73,7 @@ const CashEditInput = () => {
                 name="item_code"
                 control={control}
                 render={({ field }) => (
-                  <select {...field}>
+                  <select {...field} className="border border-brack h-10 w-44">
                     {itemsArray.map((item) => (
                       <option key={item.id} value={item.id}>
                         {item.item_name}
@@ -84,7 +88,7 @@ const CashEditInput = () => {
               <Controller
                 name="memo"
                 control={control}
-                render={({ field }) => <input type="text" {...field} />}
+                render={({ field }) => <input type="text" {...field} className="border border-brack h-10 w-44" />}
               />
             </div>
             <div>
@@ -93,7 +97,7 @@ const CashEditInput = () => {
                 name="price_type_code"
                 control={control}
                 render={({ field }) => (
-                  <select {...field}>
+                  <select {...field} className="border border-brack h-10 w-36">
                     {priceTypeArray.map((priceType) => (
                       <option key={priceType.id} value={priceType.id}>
                         {priceType.price_type}
@@ -108,7 +112,7 @@ const CashEditInput = () => {
               <Controller
                 name="price"
                 control={control}
-                render={({ field }) => <input type="text" {...field} />}
+                render={({ field }) => <input type="text" {...field} className="border border-brack h-10 w-44" />}
               />
             </div>
             <div>
@@ -122,13 +126,11 @@ const CashEditInput = () => {
               />
             </div>
           </div>
-          <div>
-            <input type="submit" value="更新" className="bg-blue-500 text-white" />
+          <div className="flex justify-center space-x-3">
+            <input type="submit" value="更新" className="border border-gray-300 bg-blue-500 hover:bg-blue-300 text-white py-4 px-6 rounded-3xl font-bold w-28" />
+            <BackButton />
           </div>
         </form>
-        <div>
-          <BackButton />
-        </div>
       </div>
     </>
   );
